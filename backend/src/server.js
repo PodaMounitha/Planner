8d0607215025c0fc601ecad66a800427ba00fc04
PodaMounitha <podamounitha.4829@gmail.com>
2026-03-22 23:22:58 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,25 +4,41 @@ import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-console.log("Starting server with correct ENV variables loaded...");
 const PORT = process.env.PORT || 5001;
+
+// ✅ IMPORTANT FIX
+const __dirname = path.resolve("../");
+
+console.log("Starting server with correct ENV variables loaded...");
 
 app.use(express.json());
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 
 app.use((req, res, next) => {
   console.log(`Req method is ${req.method} & Req url is ${req.url}`);
   next();
 });
 
-
 app.use("/api/notes", notesRoutes);
+
+// ✅ FIXED PATH
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.join(__dirname, "frontend", "dist");
+
+  app.use(express.static(distPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
 app.use(rateLimiter);
 
 connectDB().then(() => {
